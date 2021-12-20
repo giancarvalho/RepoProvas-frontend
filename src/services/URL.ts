766fd661dfc0e6bce5dd38422fe53901cc0d287d
giancarvalho <giandcarvalho@gmail.com>
2021-12-20 14:41:09 -0300

    add deployed api link to URL file

--- src/services/URL.ts
+++ src/services/URL.ts
@@ -1,4 +1,6 @@
 const URL =
-    process.env.NODE_ENV === "development" ? "http://localhost:4000" : "";
+    process.env.NODE_ENV === "development"
+        ? "http://localhost:4000"
+        : "https://api-repoprovas-1.herokuapp.com";
 
 export default URL;
