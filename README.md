ac896ba6e9a3f582ccb3d413d3c794ab795d3009
Sidney Outeiro <sid@dcc.ufrj.br>
2021-12-26 01:32:44 -0300

    preparando para o review

--- README.md
+++
@@ -1,1 +0,0 @@
-# RepoProvas-frontend
--- src/services/URL.ts
+++
@@ -1,6 +0,0 @@
-const URL =
-    process.env.NODE_ENV === "development"
-        ? "http://localhost:4000"
-        : "https://api-repoprovas-1.herokuapp.com";
-
-export default URL;
